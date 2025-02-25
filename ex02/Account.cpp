6ffa87828f1e645b8ff4fcb4bc96e64007d44ca1
aelaen <aboukezi@paul-f3Br3s6.clusters.42paris.fr>
2025-02-25 20:39:16 +0100

    en cours

---
+++ ex02/Account.cpp
@@ -0,0 +1,41 @@
+#include "Account.hpp"
+
+// int				_accountIndex = 0;
+// int				_amount = 0;
+// int				_nbDeposits = 0;
+// int				_nbWithdrawals = 0;
+
+Account::Account( int initial_deposit )
+{
+
+}
+
+Account::~Account(void)
+{
+    
+}
+
+int Account::getNbAccounts(void)
+{
+
+}
+
+static int	getTotalAmount(void)
+{
+
+}
+
+static int	getNbDeposits(void)
+{
+
+}
+
+static int	getNbWithdrawals(void)
+{
+
+}
+
+static void	displayAccountsInfos(void)
+{
+
+}
