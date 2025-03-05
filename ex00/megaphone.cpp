c885a9e7f139af0134376dd81b72043d2f44e78f
aloon <118343415+aelaen-1@users.noreply.github.com>
2025-03-05 18:22:31 +0100

    pas le droit à la STL...

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <cstring>
-#include <algorithm>
 
 // transform( InputIt first1, InputIt last1, OutputIt d_first, UnaryOp unary_op );
 // std::transform applies the given function to the elements of the given input range(s),
 // and stores the result in an output range starting from d_first
+
+// TRANSFORM INTERDIT  CAR STL 
+// std::string s[ac - 1];
+// s[i] = (const char *)argv[i];
+// std::transform(s[i].begin(), s[i].end(), s[i].begin(), ::toupper);
 
 int main(int ac, char **argv)
 {
@@ -13,12 +17,15 @@ int main(int ac, char **argv)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return (0);
     }
-    std::string s[ac - 1];
+    
     for (int i = 1; i < ac; i++)
     {
-        s[i] = (const char *)argv[i];
-        std::transform(s[i].begin(), s[i].end(), s[i].begin(), ::toupper);
-        std::cout << s[i];
+        for (size_t j = 0; j < strlen(argv[i]); j++)
+        {
+            if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+                argv[i][j] -= 32;
+        }
+        std::cout << argv[i];
     }
     std::cout << std::endl;
     return (0);
